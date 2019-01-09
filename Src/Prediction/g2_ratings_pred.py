463fcefb66cf1c27a9372de7fbe806c31e6555e0
biglifa <bigbah007@gmail.com>
2019-01-09 11:43:20 +0100

    Une fonction rebiaissage de note
    Elle remplace les NaN sur la matrice  centrée reduite pour une note donné (n) et calcul la note correspondante.

---
+++ Src/Prediction/g2_ratings_pred.py
@@ -0,0 +1,11 @@
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+
+
+def notes(df,id_user,n,id_item):
+    tqdm.pandas(desc = "Moyennes")
+    M=df.progress_apply(axis=0,func= np.mean)
+    tqdm.pandas(desc = "Ecarts types")
+    E=df.progress_apply(axis=0,func= np.std)
+    return n*E[id_item]+M[id_item]
