a5217c5839c5568b61e5743792fe694dab6df7a6
Mamadou SECK <moustaphasbt@gmail.com>
2019-01-10 14:00:27 +0100

    version 2 predit

--- Src/Prediction/predict_nan.py
+++ Src/Prediction/predict_nan.py
@@ -13,7 +13,7 @@ The function calculates and returns the k user closest to user_id
 
 '''
 
-def kkn(id_user, distance, k = "all"):
+def knn(id_user, distance, k = "all"):
     return distance[id_user].sort_values().index[1: int(k)+1 if  k != "all" else len(distance)]
 
 
@@ -28,16 +28,25 @@ The function predicts the NANs from the notes of the closest users
 
 '''
 
-def predit(df, distance):
-    user = df.index
+'''
+Function predit
+
+Input : - df DataFrame
+        - distance 
+Output : df DataFrame predit
+
+The function predicts the NANs from the notes of the closest users
+
+'''
+
+def predit(df, distance, k):
     dfold = df.copy()
     dfnew = df.copy()
-    for u in user:
-        ukkn = kkn(u, distance)
-        for uv in ukkn:
-            index = dfnew.loc[u, dfnew.loc[u].isna()].index
-            if len(index) == 0:
-                break
-            else:
-                dfnew.loc[u, index] = dfold.loc[uv, index]
+    for u in dfnew.index:
+        ukkn = knn(u, distance)
+        index = dfnew.loc[u, dfnew.loc[u].isna()].index
+        if len(index) != 0:
+            for p in index:
+                datanotna = dfold[p][dfold[p].notna()]
+                dfnew.loc[u, p] =  np.mean(datanotna[[j for j in ukkn if j in datanotna.index]][:k])
     return dfnew
