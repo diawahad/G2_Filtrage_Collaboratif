8616b41756879f5b81fefbe402bc2e3e0843c7cd
Unknown <dusart.alex@free.fr>
2019-01-10 16:22:06 +0100

    [UPDATE] Stable version prediction file

--- Src/Prediction/g2_predict_nan.py
+++ Src/Prediction/g2_predict_nan.py
@@ -1,11 +1,10 @@
-import pandas as pd
 import numpy as np
 
 '''
 Function kkn
 
 Input : - id_user user ID
-        - distance 
+        - distance
         - k user number default all
 Output : list of k closest users of user_id
 
@@ -13,20 +12,23 @@ The function calculates and returns the k user closest to user_id
 
 '''
 
-def kkn(id_user, distance, k = "all"):
-    return distance[id_user].sort_values().index[1: int(k)+1 if  k != "all" else len(distance)]
+
+def knn(id_user, distance, k="all"):
+    return distance[id_user].sort_values().index[
+            1: int(k)+1 if k != "all" else len(distance)]
 
 
 '''
 Function predit
 
 Input : - df DataFrame
-        - distance 
+        - distance
 Output : df DataFrame predit
 
 The function predicts the NANs from the notes of the closest users
 
 '''
+
 
 def predit_df(df, distance, k):
     dfold = df.copy()
@@ -37,14 +39,17 @@ def predit_df(df, distance, k):
         if len(index) > 0:
             for p in index:
                 datanotna = dfold[p][dfold[p].notna()]
-                dfnew.loc[u, p] =  np.mean(datanotna[[j for j in ukkn if j in datanotna.index]][:k])
+                dfnew.loc[u, p] = np.mean(datanotna[[
+                        j for j in ukkn if j in datanotna.index]][:k])
     return dfnew
+
 
 def predit(df, distance):
     if type(df) == 'dic':
-        return predit_dic(df,distance)
+        return predit_dic(df, distance)
     elif type(df) == 'DataFrame':
-        return predit_df(df,distance)
+        return predit_df(df, distance)
+
 
 def predit_dic(dico, distance):
     return dico
