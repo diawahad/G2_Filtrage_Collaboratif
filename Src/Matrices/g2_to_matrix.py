f85ba3a3f761477dee1d4eac24ec5ffee602953e
Corentin <corentin.lryr31@gmail.com>
2019-01-10 17:04:54 +0100

    Description of functions

--- Src/Matrices/g2_to_matrix.py
+++ Src/Matrices/g2_to_matrix.py
@@ -1,18 +1,17 @@
 """
 Created on Mon Jan  7 15:33:15 2019
 
-Group 2
-@author: Alice A, Corentin L, Charlotte M, Alexis D, Paul L, Ismail H.
+@author: Group 2 !
+
 """
 # coding: utf-8
 
+
 import pandas as pd
 import numpy as np
-from math import isnan
 from tqdm import tqdm
 from sklearn.preprocessing import scale
-import csv
-import unicodedata
+
 
 # %%
 
@@ -21,7 +20,9 @@ Function to_matrix
 
 Input : - csv rating file path
         - csv products file path
+
 Output : User/Products Rating Matrix for movies
+
 The function transforms the ratings and the products files to a matrix :
     rows : users
     columns : products
@@ -45,12 +46,16 @@ def to_matrix(filepath_rating, filepath_product, variable='user'):
     return df_join
 
 
+# %%
+
 '''
 Function to_dict
 
 Input : - csv rating file path
         - csv products file path
+
 Output : User/Products Rating dictionary for movies
+
 The function transforms the ratings and the products files to a dictionary :
     key : users
     values : set of (products,rate) tuples
@@ -79,15 +84,17 @@ def to_dict(filepath_rating, filepath_product, variable='user'):
         d_users_rates[i] = s_users
     return d_users_rates
 
-# %%
 
+# %%
 
 '''
 Function to_2d
 
 Input : - csv rating file path
         - csv products file path
+
 Output : User/Products Rating dictionary for movies
+
 The function transforms the ratings and the products files to a dictionary :
     key : users
     values : set of (products,rate) tuples
@@ -115,7 +122,6 @@ def to_2d(filepath_rating, filepath_product):
 
 # %%
 
-
 '''
 Function unbias
 
@@ -125,7 +131,9 @@ Input :
         to unbias by user instead of items
     mean - False by default, replaces missing values with 5.5 which is
         the median value in the Sens Critique rating system
+
 Output: Unbiased matrix
+
 The function centers and reduces the given dataframe, item by item
 Ignores missing  values
 '''
@@ -150,29 +158,53 @@ def unbias(df, axis=0, mean=False):
         print(ex)
 
 
+# %%
+
 '''
-arr = unbias(to_matrix("../data_v3/ratings_V3.csv","../data_v3/products_V3.csv"
-))
-#print(arr)
-for k in arr.keys():
-    print(pd.DataFrame(list(arr[k]), columns = ['item_id','rating']))'''
+Function list_genre
+
+Input :
+    filepath_rating : csv rating file path
+
+Ouput :
+    List with all the categories of movies
+
+The function return a list that includes all the differents categories of
+movies.
+'''
 
 
-#%%
+def list_genre(filepath_product):
+    df_product = pd.read_csv(filepath_product, header=0, sep=";")
+    df_product = df_product.loc[df_product['subtype_id'] == 1.0]
+    list_genres = []
+    for i in df_product['genres']:
+        if (type(i) == str):
+            r = i.split(',')
+            for j in r:
+                if j not in list_genres:
+                    list_genres.append(j)
+    list_genres.sort()
+    return list_genres
+
+
+# %%
 
 '''
 Function categories_of_movies
 
 Input :
-    filepath_rating : csv rating file path (text)
+    filepath_rating : csv rating file path
+
 Ouput :
-    DataFrameGroupBy object group by the attribute
+    DataFrame object group by the attribute
+
 The function return a matrix moviesXcategories. When a movie belongs to a
 certain categorie, we put the value 1 in the related slot instead of a 0.
 '''
 
 
-def categories_of_movies(filepath_product):
+def categories_movies(filepath_product):
     df_prod = pd.read_csv(filepath_product, header=0, sep=";")
     s = df_prod[df_prod['subtype_id'] == 1.0]
     v = s['genres']
@@ -192,88 +224,71 @@ def categories_of_movies(filepath_product):
 
     return df
 
-categories_of_movies("/home/ddm-turing3/Bureau/SensCritique/data_v3/products_V4.csv")
 
 # %%
 
 '''
-Function list_genre
+Function most_rated_movies
 
 Input :
-    filepath_rating : csv rating file path (text)
+    filepath_rating : csv rating file path
+    filepath_product : csv product file path
+    modalite (float) : - Movie : 1.0
+                       - Book : 2.0
+                       - Serie : 4.0
+    k (integer) : The k first rows of the DataFrame (Ex : 100)
+
 Ouput :
-    List with all the categories of movies
-The function return a list that includes all the differents categories of
-movies.
+    DataFrame with product_id, subtype_id(modality), rating_count
+
+The function return a DataFrame with the number of ratings and the subtype of
+the k first most rated products.
 '''
 
 
-def list_genre(filepath_product):
-    df_product = pd.read_csv(filepath_product, header=0, sep=";")
-    df_product = df_product.loc[df_product['subtype_id'] == 1.0]
-#    df_product = df_product.loc[:, ['genres']]
-    list_genres = []
-    for i in df_product['genres']:
-        if (type(i) == str):
-            r = i.split(',')
-            for j in r:
-                if j not in list_genres:
-                    list_genres.append(j)
-    list_genres.sort()
-    return list_genres
-
-# %%
-# Juste Vrai Bon Exact Correct Valide
-
-
-def to_FPN(filepath_rating, filepath_product, k):
+def most_rated_movies(filepath_rating, filepath_product, modalite, k):
     df_rating = pd.read_csv(filepath_rating, header=0, sep=";", nrows=int(1e5))
     df_prod = pd.read_csv(filepath_product, header=0, sep=";")
+
     df_join = pd.merge(df_rating, df_prod)
     df_join = df_join.loc[:, ['product_id', 'rating', 'subtype_id']]
     df_join = df_join.groupby(['product_id', 'subtype_id'],
                               as_index=False)['rating'].count()
     df_join = df_join.rename(columns={"rating": "rating_count"})
     df_join = df_join.sort_values(by='rating_count', ascending=False)
-    df_join = df_join.head(k)
-    return df_join
+    FPN = df_join.head(k)
 
-
-FPN = to_FPN("/home/ddm-turing3/Bureau/SensCritique/data_v3/ratings_V3.csv",
-             "/home/ddm-turing3/Bureau/SensCritique/data_v3/products_V4.csv",
-             100)
-
-
-# %%
-# Juste Vrai Bon Exact Correct Valide
-
-
-def to_merge_FPN(filepath_rating, FPN, modalite):
-    df_rating = pd.read_csv(filepath_rating, header=0, sep=";", nrows=int(1e5))
     df_merge = pd.merge(df_rating, FPN, on='product_id', how='left')
     df_merge = df_merge.loc[df_merge['subtype_id'] == modalite]
     df_merge = df_merge.loc[:, ['product_id', 'rating_count', 'subtype_id']]
-    df_merge = df_merge.rename(columns={"subtype_id": "modalite"})
-    df_merge = df_merge.groupby(['product_id', 'modalite'],
+    df_merge = df_merge.rename(columns={"subtype_id": "modality"})
+    df_merge = df_merge.groupby(['product_id', 'modality'],
                                 as_index=False)['rating_count'].count()
     df_merge = df_merge.sort_values(by='rating_count', ascending=False)
     return df_merge
 
 
-to_merge_FPN("/home/ddm-turing3/Bureau/SensCritique/data_v3/ratings_V3.csv",
-             FPN, 1.0)
-
-
 # %%
 
-def to_merge_FPG(filepath_rating, filepath_product):
+'''
+Function ratings_categories_movies
+
+Input :
+    filepath_rating : csv rating file path
+    filepath_product : csv product file path
+
+Ouput :
+    DataFrame with user_id, product_id, rating, categories
+
+The function return a DataFrame with the rating of an user for a movie
+describes by its categories.
+'''
+
+
+def ratings_categories_movies(filepath_rating, filepath_product):
     df_rating = pd.read_csv(filepath_rating, header=0, sep=";", nrows=int(1e3))
-    df_fpg = categories_of_movies(filepath_product)
+    df_fpg = categories_movies(filepath_product)
     df_rating = df_rating.loc[:, ['user_id', 'product_id', 'rating']]
     df_merge = pd.merge(df_rating, df_fpg, on='product_id', how='inner')
     return df_merge
-
-
-to_merge_FPG("/home/ddm-turing3/Bureau/SensCritique/data_v3/ratings_V3.csv",
-             "/home/ddm-turing3/Bureau/SensCritique/data_v3/products_V4.csv")
 
