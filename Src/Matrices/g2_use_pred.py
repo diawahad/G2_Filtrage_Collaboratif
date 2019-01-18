24d07b97a6623946462830e76f34a8bce8619ca6
Charlotte <charlotte.marque@orange.fr>
2019-01-18 09:47:38 +0100

    [update] use_pred ajout jpnb

--- Src/Matrices/g2_use_pred.py
+++ Src/Matrices/g2_use_pred.py
@@ -40,7 +40,7 @@ The function returns the predicted matrix Users/Products
 
 def collaborative_filtering(filepath_rating, filepath_product, k=1,
                             t='matrix', t_user='user', modality=1.0,
-                            rating_rows=1e3, prod_rows=1e5):
+                            rating_rows=1e3, prod_rows=1e5, jpnb=False):
     matrix_user_item = to_matrix(filepath_rating, filepath_product, t_user,
                                  modality, rating_rows, prod_rows)
     matrix_user_item_scale = pd.DataFrame(scale(matrix_user_item),
@@ -52,8 +52,8 @@ def collaborative_filtering(filepath_rating, filepath_product, k=1,
         matrice_creuse = to_dict(filepath_rating, filepath_product, t_user,
                                  modality, rating_rows, prod_rows)
         distance = similarity_user_user_dic(matrice_creuse)
-    #df_knn = predit_df(matrix_user_item, distance, k)
-    df_knn_CR = predit_df(matrix_user_item_scale, distance, k)
+    #df_knn = predit_df(matrix_user_item, distance, jpnb, k)
+    df_knn_CR = predit_df(matrix_user_item_scale, distance, jpnb, k)
     
     valpred = recalculate(matrix_user_item,df_knn_CR)
     return valpred
